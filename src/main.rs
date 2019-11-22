ae85231a5f470acd0bcac8cca053dce4e5c76fcb
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2019-11-22 01:07:53 +0100

    Minor adjust

--- src/main.rs
+++ src/main.rs
@@ -39,25 +39,22 @@ fn create_keys(
 ) -> Result<identity::Keypair, Box<dyn std::error::Error>> {
     let mut key_path = PathBuf::from(home_path);
     key_path.push(".peer_key");
+    let keypair;
     if key_path.exists() {
         let mut f = File::open(key_path)?;
         let mut key_buffer = Vec::new();
         f.read_to_end(&mut key_buffer)?;
-
         let secret = identity::secp256k1::SecretKey::from_bytes(key_buffer)?;
-        let keypair = identity::secp256k1::Keypair::from(secret);
-
-        Ok(identity::Keypair::Secp256k1(keypair))
+        keypair = identity::secp256k1::Keypair::from(secret);
     } else {
         let mut key_file = File::create(key_path)?;
-        let keypair = identity::secp256k1::Keypair::generate();
+        keypair = identity::secp256k1::Keypair::generate();
         let secret = keypair.secret();
         // TODO: use asn1_der to store keys
         // https://github.com/kizzycode/asn1_der
         key_file.write_all(&secret.to_bytes())?;
-
-        Ok(identity::Keypair::Secp256k1(keypair))
     }
+    Ok(identity::Keypair::Secp256k1(keypair))
 }
 
 fn connect(
